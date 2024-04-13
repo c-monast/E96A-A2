f75a3568764b41f36eda128a823194d537ac0148
c-monast <cameron.monast@gmail.com>
2024-04-13 15:22:49 -0700

    Added double jump functionality

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isGrounded = false;
+    private bool doubleJump;
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpHeight = 5f;
+
+    private Vector2 direction = Vector2.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 direction = value.Get<Vector2>();
+        Debug.Log(direction);
+        this.direction = direction;
+    }
+
+    void Update()
+    {
+        Move(direction.x, direction.y);
+
+        if(isGrounded && Input.GetButton("Space"))
+        {
+            doubleJump = false;
+        }
+    }
+
+    private void Move(float x, float z)
+    {
+        rb.velocity = new Vector3(x * speed, rb.velocity.y, z * speed);
+    }
+
+    void OnJump()
+    {
+        if (isGrounded || doubleJump)
+        {
+            Jump();
+            doubleJump = !doubleJump;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpHeight, rb.velocity.z);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (Vector3.Angle(collision.GetContact(0).normal, Vector3.up) < 45f)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
